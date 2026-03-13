688b61928da38d997643fc1b3524cff78de3d5f3
Your Name <you@example.com>
2026-03-14 01:08:36 +0530

    msg

--- vars/javaEKSPipeline.groovy
+++ vars/javaEKSPipeline.groovy
@@ -1,225 +1,264 @@
 // call is the default function name
-def call (Map configMap){
-    pipeline {
-    // These are pre-build sections
-        agent {
-            node {
-                label 'AGENT-1' //roboshop-java
-            }
-        }
-        environment {
-            COURSE = "Jenkins"
-            appVersion = ""
-            ACC_ID = "160885265516"
-            PROJECT = configMap.get("project")
-            COMPONENT = configMap.get("component")
-        }
-        options {
-            timeout(time: 10, unit: 'MINUTES') 
-            disableConcurrentBuilds()
-        }
-        // This is build section
-        stages {
-            stage('Read Version') {
-                steps {
-                    script{
-                        def pom = readMavenPom file: 'pom.xml'
-                        appVersion = pom.version
-                        echo "app version: ${appVersion}"
-                    }
-                }
-            }
-            stage('Install Dependencies') {
-                steps {
-                    script{
-                        sh """
-                            mvn clean package
-                        """
-                    }
-                }
-            }
-            stage('Unit Test') {
-                steps {
-                    script{
-                        sh """
-                            echo test
-                        """
-                    }
-                }
-            }
-            // //Here you need to select scanner tool and send the analysis to server
-            // /* stage('Sonar Scan'){
-            //     environment {
-            //         def scannerHome = tool 'sonar-8.0'
-            //     }
-            //     steps {
-            //         script{
-            //             withSonarQubeEnv('sonar-server') {
-            //                 sh  "${scannerHome}/bin/sonar-scanner"
-            //             }
-            //         }
-            //     }
-            // }
-            // stage('Quality Gate') {
-            //     steps {
-            //         timeout(time: 1, unit: 'HOURS') {
-            //             // Wait for the quality gate status
-            //             // abortPipeline: true will fail the Jenkins job if the quality gate is 'FAILED'
-            //             waitForQualityGate abortPipeline: true 
-            //         }
-            //     }
-            // } */
+def call(Map configMap){
 
-        //     stage('Snyk Dependency Scan') {
-        //         environment {
-        //             SNYK_TOKEN = credentials('snyk-token')
-        //         }
-        //         steps {
-        //             script{
-        //                 sh """
-        //                     npm install -g snyk
-        //                     snyk auth ${SNYK_TOKEN}
-        //                     snyk test --severity-threshold=high
-        //                 """
-        //             }
-        //         }
-        //     }
-            
+pipeline {
 
-
-        // stage('OWASP ZAP DAST Scan') {
-        //     steps {
-        //         script{
-        //             sh """
-        //                 docker run -t owasp/zap2docker-stable zap-baseline.py \
-        //                 -t http://dev.${COMPONENT}.yourdomain.com \
-        //                 -r zap-report.html
-        //             """
-        //         }
-        //     }
-        // }
-
-            stage('Dependabot Security Gate') {
-                when {
-                    expression { false }
-                }
-                environment {
-                    GITHUB_OWNER = 'daws-86s'
-                    GITHUB_REPO  = 'catalogue'
-                    GITHUB_API   = 'https://api.github.com'
-                    GITHUB_TOKEN = credentials('GITHUB_TOKEN')
-                }
-
-                steps {
-                    script{
-                        /* Use sh """ when you want to use Groovy variables inside the shell.
-                        Use sh ''' when you want the script to be treated as pure shell. */
-                        sh '''
-                        echo "Fetching Dependabot alerts..."
-
-                        response=$(curl -s \
-                            -H "Authorization: token ${GITHUB_TOKEN}" \
-                            -H "Accept: application/vnd.github+json" \
-                            "${GITHUB_API}/repos/${GITHUB_OWNER}/${GITHUB_REPO}/dependabot/alerts?per_page=100")
-
-                        echo "${response}" > dependabot_alerts.json
-
-                        high_critical_open_count=$(echo "${response}" | jq '[.[] 
-                            | select(
-                                .state == "open"
-                                and (.security_advisory.severity == "high"
-                                    or .security_advisory.severity == "critical")
-                            )
-                        ] | length')
-
-                        echo "Open HIGH/CRITICAL Dependabot alerts: ${high_critical_open_count}"
-
-                        if [ "${high_critical_open_count}" -gt 0 ]; then
-                            echo "❌ Blocking pipeline due to OPEN HIGH/CRITICAL Dependabot alerts"
-                            echo "Affected dependencies:"
-                            echo "$response" | jq '.[] 
-                            | select(.state=="open" 
-                            and (.security_advisory.severity=="high" 
-                            or .security_advisory.severity=="critical"))
-                            | {dependency: .dependency.package.name, severity: .security_advisory.severity, advisory: .security_advisory.summary}'
-                            exit 1
-                        else
-                            echo "✅ No OPEN HIGH/CRITICAL Dependabot alerts found"
-                        fi
-                        '''
-                        
-                    }
-                }
-            }
-
-            stage('Build Image') {
-                steps {
-                    script{
-                        withAWS(region:'us-east-1',credentials:'aws-creds') {
-                            sh """
-                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
-                                docker images
-                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                            """
-                        }
-                    }
-                }
-            }
-            
-            /* stage('Trivy Scan'){
-                steps {
-                    script{
-                        sh """
-                            trivy image \
-                            --scanners vuln \
-                            --severity HIGH,CRITICAL,MEDIUM \
-                            --pkg-types os \
-                            --exit-code 1 \
-                            --format table \
-                            ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
-                        """
-                    }
-                }
-            } */
-
-            stage('Trigger DEV Deploy') {
-                steps {
-                    script {
-                        build job: "../${COMPONENT}-deploy",
-                            wait: false, // Wait for completion
-                            propagate: false, // Propagate status
-                            parameters: [
-                                string(name: 'appVersion', value: "${appVersion}"),
-                                string(name: 'deploy_to', value: "dev")
-                            ]
-                    }
-                }
-            }
-
-        }
-
-            
-
-        post{
-            always{
-                echo 'I will always say Hello again!'
-                cleanWs()
-            }
-            
-           always {
-           archiveArtifacts artifacts: 'zap-report.html', fingerprint: true
-           }
-
-            success {
-                echo 'I will run if success'
-            }
-            failure {
-                echo 'I will run if failure'
-            }
-            aborted {
-                echo 'pipeline is aborted'
-            }
+    agent {
+        node {
+            label 'AGENT-1'
         }
     }
+
+    environment {
+        ACC_ID = "160885265516"
+        appVersion = ""
+        PROJECT = configMap.get("project")
+        COMPONENT = configMap.get("component")
+    }
+
+    options {
+        timeout(time: 30, unit: 'MINUTES')
+        disableConcurrentBuilds()
+    }
+
+    stages {
+
+        stage('Read Version') {
+            steps {
+                script{
+                    def pom = readMavenPom file: 'pom.xml'
+                    appVersion = pom.version
+                    echo "Application Version: ${appVersion}"
+                }
+            }
+        }
+
+        stage('Build Application') {
+            steps {
+                script{
+                    sh '''
+                    mvn clean package
+                    '''
+                }
+            }
+        }
+
+        stage('Unit Test') {
+            steps {
+                script{
+                    sh '''
+                    mvn test
+                    '''
+                }
+            }
+        }
+
+        
+
+        stage('Snyk Dependency Scan') {
+
+            environment {
+                SNYK_TOKEN = credentials('snyk-token')
+            }
+
+            steps {
+
+                script{
+
+                    sh '''
+                    curl -Lo snyk https://static.snyk.io/cli/latest/snyk-linux
+                    chmod +x snyk
+
+                    ./snyk auth $SNYK_TOKEN
+
+                    ./snyk test \
+                    --file=pom.xml \
+                    --severity-threshold=high
+                    '''
+                }
+
+            }
+
+        }
+
+    
+
+        stage('SonarQube Scan') {
+
+            steps {
+
+                script{
+
+                    def scannerHome = tool 'sonar-8.0'
+
+                    withSonarQubeEnv('sonar-server') {
+
+                        sh """
+                        ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${COMPONENT} \
+                        -Dsonar.sources=.
+                        """
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        stage('Quality Gate') {
+
+            steps {
+
+                timeout(time: 1, unit: 'HOURS') {
+
+                    waitForQualityGate abortPipeline: true
+
+                }
+
+            }
+
+        }
+
+   
+
+        stage('Build Docker Image') {
+
+            steps {
+
+                script{
+
+                    withAWS(region:'us-east-1',credentials:'aws-creds') {
+
+                        sh """
+
+                        aws ecr get-login-password \
+                        --region us-east-1 | docker login \
+                        --username AWS \
+                        --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+
+                        docker build \
+                        -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+
+                        docker push \
+                        ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+
+                        """
+
+                    }
+
+                }
+
+            }
+
+        }
+
+   
+
+        stage('Trivy Image Scan') {
+
+            steps {
+
+                script{
+
+                    sh """
+
+                    trivy image \
+                    --severity HIGH,CRITICAL \
+                    --exit-code 1 \
+                    ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+
+                    """
+
+                }
+
+            }
+
+        }
+
+   
+        stage('Deploy to DEV') {
+
+            steps {
+
+                script {
+
+                    build job: "../${COMPONENT}-deploy",
+
+                    wait: false,
+                    propagate: false,
+
+                    parameters: [
+
+                        string(name: 'appVersion', value: "${appVersion}"),
+                        string(name: 'deploy_to', value: "dev")
+
+                    ]
+
+                }
+
+            }
+
+        }
+
+        // -------------------------
+        // DAST Scan
+        // -------------------------
+
+        stage('OWASP ZAP DAST Scan') {
+
+            steps {
+
+                script{
+
+                    sh """
+
+                    docker run -t owasp/zap2docker-stable zap-baseline.py \
+                    -t http://dev.${COMPONENT}.yourdomain.com \
+                    -r zap-report.html
+
+                    """
+
+                }
+
+            }
+
+        }
+
+    }
+
+    post {
+
+        always {
+
+            archiveArtifacts artifacts: 'zap-report.html', fingerprint: true
+
+            cleanWs()
+
+            echo 'Pipeline Completed'
+
+        }
+
+        success {
+
+            echo 'Build Successful'
+
+        }
+
+        failure {
+
+            echo 'Build Failed'
+
+        }
+
+        aborted {
+
+            echo 'Pipeline Aborted'
+
+        }
+
+    }
+
+}
 }
